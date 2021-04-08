bcd5b1fabb5c7b66dd3eeab07e8b2ead058c23e8
Edgar Pena <edgarpena0740@gmail.com>
2021-04-08 12:48:35 -0700

    rearranced form into one column and button into another// Working on changing width of blank columns

--- client/src/Components/Main/Main.js
+++ client/src/Components/Main/Main.js
@@ -30,51 +30,49 @@ const Main = () => {
             <h1>Find Your Path</h1>
           </div>
         </Row>
-        <Row className="formRow">
-          {/* <Col
-            className="icon d-flex align-items-center justify-content-center"
-            xs="1"
-            sm="1"
-            md="1"
-            lg="1"
+        <Row className="formRow d-flex align-items-stretch">
+          {/* Blank */}
+          <Col sm="1" md="1" lg="1" className="blankColLeft"></Col>
+          {/* blank */}
+          {/* Start of form col*/}
+          <Col
+            sm="9"
+            md="9"
+            lg="9"
+            className="formCol d-flex align-items-center"
           >
-            <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30">
-                <path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z" />
-              </svg>
-            </div>
-          </Col> */}
-          {/* <Col
-            className="blankCol d-flex align-items-stretch"
-            xs="1"
-            sm="1"
-            md="1"
-            lg="1"
-          >
-            {" "}
-          </Col> */}
-          <Col className="form d-flex align-items-center">
-            <Form className="searchForm">
-              <Form.Control
-                className="formControl form-control-lg shadow-none"
+            <Form className="form-inline searchForm">
+              <input
                 type="text"
+                className="form-control formInput form-control-lg"
+                id="inputSearch"
                 placeholder="Search"
-              ></Form.Control>
+              ></input>
             </Form>
           </Col>
+          {/* End of form */}
+          {/* Start of Button col */}
           <Col
-            className="buttonCol d-flex align-items-stretch"
-            xs="1"
             sm="1"
             md="1"
             lg="1"
+            className="btnCol border-left d-flex align-items-center justify-content-center "
           >
-            <Button type="submit" form="searchForm" className="searchBtn">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30">
-                <path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z" />
+            <button type="submit" className="btn submitBtn" form="inputSearch">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M23.111 20.058l-4.977-4.977c.965-1.52 1.523-3.322 1.523-5.251 0-5.42-4.409-9.83-9.829-9.83-5.42 0-9.828 4.41-9.828 9.83s4.408 9.83 9.829 9.83c1.834 0 3.552-.505 5.022-1.383l5.021 5.021c2.144 2.141 5.384-1.096 3.239-3.24zm-20.064-10.228c0-3.739 3.043-6.782 6.782-6.782s6.782 3.042 6.782 6.782-3.043 6.782-6.782 6.782-6.782-3.043-6.782-6.782zm2.01-1.764c1.984-4.599 8.664-4.066 9.922.749-2.534-2.974-6.993-3.294-9.922-.749z" />
               </svg>
-            </Button>
+            </button>
           </Col>
+          {/* End of button col */}
+          {/* Blank */}
+          <Col sm="1" md="1" lg="1" className="blankColRight"></Col>
+          {/* Blank */}
         </Row>
       </Container>
       {/* search bar/ form stuff */}
