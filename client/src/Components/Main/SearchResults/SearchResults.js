18974ff698bdd2419205601a59ed630dca8ed566
Edgar Pena <edgarpena0740@gmail.com>
2021-05-01 17:31:44 -0700

    went back to having seperate states in SearchResults.js

--- client/src/Components/Main/SearchResults/SearchResults.js
+++ client/src/Components/Main/SearchResults/SearchResults.js
@@ -31,26 +31,43 @@ const SearchResults = (props) => {
   // It updates the state of what the user is searching(When is get the search bar done)
   const { userSearch, setUserSearch } = useContext(QueryContext);
 
-  const [manyState, setManyState] = useState({
-    // New user search for this component
-    userInput: "",
-    // The data from the search api call
-    searchData: [],
-    // The id for the item that was clicked on
-    listItemID: "", //This is used to et the data below
-    // The Data for the list item that was clicked on
-    listItemData: [],
-    // Latitude and longitude data from mapping the search data
-    latLon: [],
-    //The location data after processing latLon in an api call
-    locationData: [],
-  });
+  // const [manyState, setManyState] = useState({
+  //   // New user search for this component
+  //   userInput: "",
+  //   // The data from the search api call
+  //   searchData: [],
+  //   // The id for the item that was clicked on
+  //   listItemID: "", //This is used to et the data below
+  //   // The Data for the list item that was clicked on
+  //   listItemData: [],
+  //   // Latitude and longitude data from mapping the search data
+  //   latLon: [],
+  //   //The location data after processing latLon in an api call
+  //   locationData: [],
+  //   hello: "",
+  // });
+
+  // Stores and is used to render the search query from IntroMain.js component(first page user sees)
+  const [searchData, setSearchData] = useState([]);
+
+  // State for saving the list item id that was clicked on
+  const [listItemID, setListItemID] = useState("");
+  // console.log(listItemID, "listItemID outside on onClick");
+
+  //State for the data based on the list item id
+  const [listItemData, setListItemData] = useState([]);
+  // console.log(listItemData, "before onclick list item data");
+
+  //state for latitude and longitude
+  const [latLonState, setLatLonState] = useState([]);
 
   const source = axios.CancelToken.source();
   let searchResArray = [];
+  let listItemFilter = [];
 
   //Since useEffect uses componentDidMount under the hood it is rerendering twice
   useEffect(() => {
+    console.log("useEffect");
     try {
       let latLonArray = [];
       axios
@@ -72,114 +89,53 @@ const SearchResults = (props) => {
             });
             console.log(searchResArray);
             console.log(latLonArray);
-            setManyState({
-              searchData: searchResArray,
-              latLon: latLonArray,
-              listItemData: [resData[0]],
-            });
+            setSearchData(searchResArray);
+            setLatLonState(latLonArray);
+            setListItemData([resData[0]]);
+            console.log(latLonState);
           }
         });
+      // return API.getLocation(many);
     } catch (error) {
       console.log(error);
     }
     return () => {
       console.log("unmounted");
-      console.log(manyState);
+
       source.cancel("was cancelled");
       console.log("hello");
+      listItemFilter = [];
     };
-  }, []);
+  }, [userSearch.input]);
 
-  const testEle = () => {
-    if (manyState.latLon !== undefined || manyState.latLon !== []) {
-      return <h1>hello</h1>;
+  const handleSearch = async (e) => {
+    try {
+      e.preventDefault();
+      // console.log("handleSearch ran");
+    } catch (error) {
+      return console.log(error);
     }
   };
 
-  const listItems = () => {
-    if (manyState.searchData !== undefined || manyState.searchData !== []) {
-      return manyState.searchData.map((result) => (
-        //
-        //
-        // *** set point-events to non in order to make this one cohesive clickable item
-        // List items for results
-        <ListGroup.Item
-          id={result.id}
-          key={result.id}
-          // onClick={(e) => onClickItem(e)}
-          type="button"
-          action
-          className="listItemStyle"
-        >
-          <Image
-            className="listItemImg d-inline"
-            src={result.images[0].url === "" ? fern : result.images[0].url}
-            alt="No Image Available"
-          />
-          <div className="listItemTitle d-inline">{result.title}</div>
-        </ListGroup.Item>
-      ));
-    }
+  const onChange = (e) => {
+    // console.log("onChange ran");
+    setUserSearch({ ...userSearch, [e.target.name]: e.target.value });
   };
 
-  // const infoItemMap = manyState.listItemData.map((data) => (
-  //   <div key={data.id}>
-  //     <h1>{data.title}</h1>
-
-  //     <Image
-  //       src={data.images[0].url === "" ? fern : data.images[0].url}
-  //       fluid
-  //     />
-  //     <div style={{ display: "none" }}>Hello World</div>
-  //     <p>{data.audioDescription}</p>
-  //     {"\n"}
-  //     <p>
-  //       {data.isOpenToPublic === "1" ? "Open to public" : "Not open to public"}
-  //     </p>
-  //     <div>
-  //       <a href={data.url}>See More Information</a>
-  //     </div>
-  //     {/* <div>{data.bodyText}</div> */}
-  //   </div>
-  // ));
-  // const infoItem = () => {
-  //   if (manyState.listItemData !== null) {
-  //     return infoItemMap;
-  //   }
-  // };
-
-  // const handleSearch = async (e) => {
-  //   try {
-  //     e.preventDefault();
-  //     // console.log("handleSearch ran");
-  //     API.searchRes(userSearch.input).then((res) => {
-  //       setSearchData(res.data.data);
-  //       setListItemData([res.data.data[0]]);
-  //     });
-  //   } catch (error) {
-  //     return console.log(error);
-  //   }
-  // };
-
-  // const onChange = (e) => {
-  //   // console.log("onChange ran");
-  //   setUserSearch({ ...userSearch, [e.target.name]: e.target.value });
-  // };
-
-  // const onClickItem = async (e) => {
-  //   console.log("OnCLickItem ran");
-  //   try {
-  //     e.preventDefault();
-  //     // console.log(e.target.id, "ID inside of onClick");
-  //     let idTarget = e.target.id;
-  //     // console.log(idTest, "idTest value");
-  //     // console.log(searchData, "search Data inside of onCLick");
-  //     let listItemFilter = searchData.filter((data) => data.id === idTarget);
-  //     setListItemData(listItemFilter);
-  //   } catch (error) {
-  //     return console.log(error);
-  //   }
-  // };
+  const onClickItem = async (e) => {
+    console.log("OnCLickItem ran");
+    try {
+      e.preventDefault();
+      // console.log(e.target.id, "ID inside of onClick");
+      let idTarget = e.target.id;
+      // console.log(idTest, "idTest value");
+      // console.log(searchData, "search Data inside of onCLick");
+      let listItemFilter = searchData.filter((data) => data.id === idTarget);
+      setListItemData(listItemFilter);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="searchResultsDiv">
@@ -196,7 +152,7 @@ const SearchResults = (props) => {
                 Name
               </Form.Label> */}
               <Form.Control
-                // onChange={onChange}
+                onChange={onChange}
                 type="text"
                 name="input"
                 id="searchInput"
@@ -213,15 +169,43 @@ const SearchResults = (props) => {
           <Col className="leftSide" lg="6" md="6">
             <ListGroup className="resultsDiv">
               <h1>Search: Hello</h1>
-              {listItems()}
+              {searchData !== undefined
+                ? searchData.map((result) => (
+                    //
+                    //
+                    // *** set point-events to non in order to make this one cohesive clickable item
+                    // List items for results
+                    <ListGroup.Item
+                      id={result.id}
+                      key={result.id}
+                      onClick={(e) => onClickItem(e)}
+                      type="button"
+                      action
+                      className="listItemStyle"
+                    >
+                      <Image
+                        className="listItemImg d-inline"
+                        src={
+                          result.images[0].url === ""
+                            ? fern
+                            : result.images[0].url
+                        }
+                        alt="No Image Available"
+                      />
+                      <div className="listItemTitle d-inline">
+                        {result.title}
+                      </div>
+                    </ListGroup.Item>
+                  ))
+                : ""}
             </ListGroup>
           </Col>
           {/*  */}
           {/* Right Side */}
           <Col className="rightSide" lg="6" md="6">
             <div className="infoDiv">
-              {manyState.listItemData !== undefined
-                ? manyState.listItemData.map((data) => (
+              {listItemData !== undefined
+                ? listItemData.map((data) => (
                     <div key={data.id}>
                       <h1>{data.title}</h1>
 
