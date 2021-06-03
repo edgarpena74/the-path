52ad4de600a9ff499ccca8de9ce240ea61775a3b
Edgar Pena <edgarpena0740@gmail.com>
2021-06-03 15:49:44 -0700

    added Row to searchResults line 152// changed css in search results as well

--- client/src/Components/Main/SearchResults/SearchResults.js
+++ client/src/Components/Main/SearchResults/SearchResults.js
@@ -149,27 +149,30 @@ const SearchResults = () => {
       <Container className="searchResultsContainer">
         {/* Search bar*/}
         {/* <Form onSubmit={handleSearch}> */}
-        <Form onSubmit={handleSearch}>
-          <Form.Row className="align-items-center">
-            <Col xs="auto">
-              {/* <Form.Label htmlFor="inlineFormInput" srOnly>
+        <Row>
+          <Form onSubmit={handleSearch}>
+            <Form.Row className="align-items-center">
+              <Col xs="auto">
+                {/* <Form.Label htmlFor="inlineFormInput" srOnly>
                 Name
               </Form.Label> */}
-              <Form.Control
-                // onClick={(e) => {
-                //   onClick(e);
-                // }}
-                type="text"
-                name="input"
-                id="searchInput"
-                placeholder="Search"
-              />
-            </Col>
-            <Col xs="auto">
-              <Button type="submit">Search</Button>
-            </Col>
-          </Form.Row>
-        </Form>
+                <Form.Control
+                  // onClick={(e) => {
+                  //   onClick(e);
+                  // }}
+                  type="text"
+                  name="input"
+                  id="searchInput"
+                  placeholder="Search"
+                />
+              </Col>
+              <Col xs="auto">
+                <Button type="submit">Search</Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Row>
+
         <Row>
           {/* Left Side */}
           <Col className="leftSide" lg="6" md="6">
