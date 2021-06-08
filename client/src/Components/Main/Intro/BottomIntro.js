c924ed9717c39f46021d14f25173bc2fde1ec35a
Edgar Pena <edgarpena0740@gmail.com>
2021-06-08 15:22:23 -0700

    added width responsive css

--- client/src/Components/Main/Intro/BottomIntro.js
+++ client/src/Components/Main/Intro/BottomIntro.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
+import Col from "react-bootstrap/Col";
 // import Image from "react-bootstrap/Image";
 import "./BottomMain.css";
 import API from "../../../utils/API";
@@ -37,29 +37,32 @@ const BottomIntro = () => {
 
   return (
     // <div className="subDiv d-flex align-items-stretch">
-    <div className="d-flex">
-      <div className="container-fluid bottomDiv">
-        <Container className="subContainer ">
-          <Row className="subHeading ">
-            <h1>Northern California Favorites</h1>
-          </Row>
-          {/* <Row className="subBoxRow d-flex justify-content-center"> */}
-          <Row className="subBoxRow d-flex justify-content-center">
-            {seeds.map((data) => (
-              <Card key={data.id} style={{ width: "15rem", margin: "7px" }}>
-                <Card.Body>
-                  <Card.Text className="text-center">{data.title}</Card.Text>
-                </Card.Body>
-                <Card.Img
-                  style={{ height: "178px" }}
-                  variant="bottom"
-                  src={data.imageURL}
-                />
-              </Card>
-            ))}
-          </Row>
-        </Container>
-      </div>
+    //
+    // original div
+    // <div className="d-flex botIntParDiv">
+    // test div
+    <div className="botIntParDiv">
+      <Container className="  bottomContainer ">
+        <Row className="subHeading ">
+          <Col className="headingCol">
+            <h1 className="heading d-flex">Northern California Favorites</h1>
+          </Col>
+        </Row>
+        <Row className="subBoxRow">
+          {seeds.map((data) => (
+            <Card key={data.id} style={{ width: "15rem", margin: "7px" }}>
+              <Card.Body>
+                <Card.Text className="text-center">{data.title}</Card.Text>
+              </Card.Body>
+              <Card.Img
+                style={{ height: "178px" }}
+                variant="bottom"
+                src={data.imageURL}
+              />
+            </Card>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
