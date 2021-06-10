5b6ce743332e837966ca82c70895537d93870a79
Edgar Pena <edgarpena0740@gmail.com>
2021-06-10 13:52:17 -0700

    added path resolve in server.js

--- server.js
+++ server.js
@@ -19,11 +19,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// if (process.env.NODE_ENV === "production") {
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/trails",
