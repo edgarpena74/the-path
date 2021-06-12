6a2edea4187910e7da017030934fbabef513f171
Edgar Pena <edgarpena0740@gmail.com>
2021-06-12 16:03:03 -0700

    trying to resolve bug in heroku deployment when going back a page

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use("/api", require("./routes/apiRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 //***************************************************** */
 // MongoDB Cluster Issue
 //
